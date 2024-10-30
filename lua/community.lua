41c96b2123ddeb4d3b07e1bb44d76343af1f7efe
Albano Maywitz <allbano@gmail.com>
2024-10-30 20:20:50 -0300

    feat: add tokyonight and catppucin colorscheme.

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
---if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -10,5 +8,8 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   -- heirline
-  { import = "astrocommunity.recipes.heirline-vscode-winbar" }, 
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  -- Colorscheme - Tokyonight
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
 }
